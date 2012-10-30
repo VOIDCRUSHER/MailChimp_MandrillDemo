19ac91928d03c4813a45f30b8b6875eab046cfd0
Cesar Flores <cgonzalezflores1@gmail.com>
2012-10-30 04:26:03 -0400

    modified demo to work with MandrillMail.py

--- src/smtpdemo.py
+++ src/smtpdemo.py
@@ -1,58 +1,20 @@
 import os
-import smtplib
-
-from email.mime.multipart import MIMEMultipart
-from email.mime.text import MIMEText
-from email.mime.base import MIMEBase
-from email.Utils import COMMASPACE
-from email import Encoders
-
-def attachFiles(message,filepaths):
-    for file in filepaths:
-        attachment = MIMEBase('application',"octet-stream")
-        attachment.set_payload(open(file,'rb').read())
-        Encoders.encode_base64(attachment)
-        attachment.add_header('Content-Disposition','attachment; filename="%s"'%os.path.basename(file))
-        message.attach(attachment)
-
-def loadMessageTemplate(filename,msgType = 'html'):
-    f = open(filename,'r')
-    msgbody = f.read()
-    message = MIMEText(msgbody,msgType)
-    return message
-
-def loadMessageRecipients(filename):
-    f = open(filename,'r')
-    recipients = [recipient for recipient in f]
-    return recipients
+import MandrillMail
         
 
 def main():
     print 'SWOOP'
-    msg = MIMEMultipart('alternative')
-    
-    msg['Subject'] = 'Oppa Mandrill Style!'
-    msg['From'] = 'Cesar Flores <cgonzalezflores1@gmail.com>' #sender_name<email_address>
-    msg['To'] = ['cgonzalez6@gatech.edu']
-    
-    text = "Heyyy Sexy Raaady! (Plaintext Example)"
-    part1 = MIMEText(text,'plain')
-    
-    html = '<em>You know what Im <strong>sayin</strong></em>(HTML Demo)'
-    part2 = MIMEText(html,'html')
     
     username = os.environ['MANDRILL_USERNAME']
     password = os.environ['MANDRILL_PASSWORD']
     
-    msg.attach(part1)
-    msg.attach(part2)
+    sender = 'Cesar Flores <cgonzalezflores1@gmail.com>' #sender_name<email_address>
+    recipient_src = 'recipients.txt'
+    subject = 'HEYYY, SEXY MONKEY'
+    template_src = 'HTemailtemplate.txt'
+    attachment_src = 'attachments.txt'
     
-    s = smtplib.SMTP('smtp.mandrillapp.com',587)
-    
-    s.login(username,password)
-    s.sendmail(msg['From'],msg['To'],msg.as_string())
-    
-    s.quit()
+    MandrillMail.sendMessageSMTP(username, password, sender, recipient_src, subject, template_src, attachment_src)
     print 'SWAG'
 
 if __name__ == "__main__":
